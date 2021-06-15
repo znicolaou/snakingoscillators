228608134822aeef5ac01a9518a9751e6b852eb5
Zachary Nicolaou <zack.nicolaou@gmail.com>
2021-06-15 01:58:40 -0700

    updated continuation

--- janus.py
+++ janus.py
@@ -9,69 +9,30 @@ from scipy.fft import fft2,ifft2
 import argparse
 
 ###########################################################################
-def janusrot(t, phases, N, omega, sigma, beta, gamma):
-    x=np.concatenate([[1],phases[:N-1]])
-    y=np.concatenate([[0],phases[N-1:2*(N-1)]])
-    u=phases[2*(N-1):2*(N-1)+N]
-    v=phases[2*(N-1)+N:]
-    dxdt=-y*(-beta*(-x*v+y*u+v[0])-sigma*(-x*np.roll(v,1)+y*np.roll(u,1)+v[1]))+gamma*(1-x**2-y**2)*x
-    dydt= x*(-beta*(-x*v+y*u+v[0])-sigma*(-x*np.roll(v,1)+y*np.roll(u,1)+v[1]))+gamma*(1-x**2-y**2)*y
-    dudt=-v*(-omega-beta*(x*v-y*u+v[0])-sigma*(np.roll(x,-1)*v-np.roll(y,-1)*u+v[1]))+gamma*(1-u**2-v**2)*u
-    dvdt= u*(-omega-beta*(x*v-y*u+v[0])-sigma*(np.roll(x,-1)*v-np.roll(y,-1)*u+v[1]))+gamma*(1-u**2-v**2)*v
-    return np.concatenate([dxdt[1:],dydt[1:],dudt,dvdt])
+def janus(t, phases, N, omega, sigma, beta, gamma, sigma0, t0):
+    sigmat=sigma
+    if (t < t0):
+        sigmat = sigma0 + (sigma - sigma0) * t / t0
+
+    x=phases[:N]
+    y=phases[N:2*N]
+    u=phases[2*N:3*N]
+    v=phases[3*N:4*N]
+    dxdt=-y*(omega/2-beta*(-x*v+y*u)-sigmat*(-x*np.roll(v,-1)+y*np.roll(u,-1)))+gamma*(1-x**2-y**2)*x
+    dydt= x*(omega/2-beta*(-x*v+y*u)-sigmat*(-x*np.roll(v,-1)+y*np.roll(u,-1)))+gamma*(1-x**2-y**2)*y
+    dudt=-v*(-omega/2-beta*(x*v-y*u)-sigmat*(np.roll(x,1)*v-np.roll(y,1)*u))+gamma*(1-u**2-v**2)*u
+    dvdt= u*(-omega/2-beta*(x*v-y*u)-sigmat*(np.roll(x,1)*v-np.roll(y,1)*u))+gamma*(1-u**2-v**2)*v
+    return np.concatenate([dxdt,dydt,dudt,dvdt])
 ###########################################################################
-def runsim (N, t1, t3, dt, omega, beta, sigma, gamma, seed, filebase, output):
-    start = timeit.default_timer()
-    # Initial phases, from file if it exists
-    np.random.seed(seed)
-    phase_init = np.zeros(4*N-2,dtype=np.float64)
 
-    if os.path.isfile(filebase + 'ic.npy'):
-        phase_init = np.concatenate(np.load(filebase + 'ic.npy'))
-        if output>0:
-            print('using initial conditions from file',flush=True)
-
-    elif os.path.isfile(filebase + 'ic.dat'):
-        phase_init = np.fromfile(filebase + 'ic.dat',dtype=np.float64)
-        if output>0:
-            print('using initial conditions from file',flush=True)
-
-    else:
-        phi0=-np.pi+2*np.pi*np.random.random(N-1)
-        phi1=-np.pi+2*np.pi*np.random.random(N)
-        phase_init[:N-1] = np.cos(phi0)
-        phase_init[N-1:2*(N-1)] = np.sin(phi0)
-        phase_init[2*(N-1):2*(N-1)+N] = np.cos(phi1)
-        phase_init[2*(N-1)+N:] = np.sin(phi1)
-        if output>0:
-            print('using random initial conditions',flush=True)
-
-    phases=np.zeros( [int(t1/dt), 4*N-2], dtype=np.float64 )
-    phase=phase_init
-
-    sol=solve_ivp(janusrot, [0,t1], phase, method='RK45', args=(N,omega, sigma, beta, gamma), rtol=1e-6, atol=1e-6, t_eval=dt*np.arange(t1/dt))
+###########################################################################
+def runsim (N, t1, t3, dt, omega, beta, sigma, gamma, phase_init, sigma0=0.35, t0=0):
+    sol=solve_ivp(janus, [0,t1], phase_init, method='RK45', args=(N,omega, sigma, beta, gamma, sigma0, t0), rtol=1e-6, atol=1e-6, t_eval=dt*np.arange(t1/dt))
     phases=sol.y.T.copy()
     times=sol.t
+    r=np.abs(np.sum(phases[:,:N]+1j*phases[:,N:2*N],axis=1)+np.sum(phases[:,2*N:3*N]+1j*phases[:,3*N:4*N],axis=1))/(2*N)
 
-    r=np.abs(1+np.sum(phases[:,:N-1]+1j*phases[:,N-1:2*(N-1)],axis=1)+np.sum(phases[:,2*(N-1):2*(N-1)+N]+1j*phases[:,2*(N-1)+N:],axis=1))/(2*N)
-
-    # Output
-    np.save(filebase+'times.npy', times)
-    np.save(filebase+'order.npy', [np.arange(0,t1,dt), r])
-    np.save(filebase+'fs.npy', phases[-1, :])
-    stop = timeit.default_timer()
-
-    if output>0:
-        print(filebase, np.mean(r[int(t3 / dt):]))
-        print('runtime: %f' % (stop - start))
-
-    if output>1:
-        np.save(filebase+'phases.npy', phases[int(t3/dt):])
-
-    f = open(filebase + 'out.dat', 'w')
-    print(*(sys.argv), sep=' ', file=f)
-    print(stop - start, np.mean(r[int(t3 / dt):]), file=f)
-    f.close()
+    return phases,times,r
 ######################################################################################
 
 if __name__ == "__main__":
@@ -107,4 +68,45 @@ if __name__ == "__main__":
     if t3>t1:
         t3=t1-dt
 
-    runsim(N, t1, t3, dt, omega, beta, sigma, gamma, seed, filebase, output)
+
+    # Initial phases, from file if it exists
+    np.random.seed(seed)
+    phase_init = np.zeros(4*N,dtype=np.float64)
+
+    if os.path.isfile(filebase + 'ic.npy'):
+        phase_init = np.concatenate(np.load(filebase + 'ic.npy'))
+        if output>0:
+            print('using initial conditions from file',flush=True)
+
+    elif os.path.isfile(filebase + 'ic.dat'):
+        phase_init = np.fromfile(filebase + 'ic.dat',dtype=np.float64)
+        if output>0:
+            print('using initial conditions from file',flush=True)
+
+    else:
+        phi0=-np.pi+2*np.pi*np.random.random(N)
+        phi1=-np.pi+2*np.pi*np.random.random(N)
+        phase_init[:N] = np.cos(phi0)
+        phase_init[N:2*N] = np.sin(phi0)
+        phase_init[2*N:3*N] = np.cos(phi1)
+        phase_init[3*N:] = np.sin(phi1)
+        if output>0:
+            print('using random initial conditions',flush=True)
+
+    start = timeit.default_timer()
+    phases,times,r=runsim(N, t1, t3, dt, omega, beta, sigma, gamma, phase_init)
+    stop = timeit.default_timer()
+
+    # Output
+    if output>0:
+        np.save(filebase+'times.npy', times)
+        np.save(filebase+'order.npy', r)
+        np.save(filebase+'phases.npy', phases[int(t3/dt):])
+        print(filebase, np.mean(r[int(t3 / dt):]))
+        print('runtime: %f' % (stop - start))
+
+    np.save(filebase+'fs.npy', phases[-1, :])
+    f = open(filebase + 'out.dat', 'w')
+    print(*(sys.argv), sep=' ', file=f)
+    print(stop - start, np.mean(r[int(t3 / dt):]), file=f)
+    f.close()
