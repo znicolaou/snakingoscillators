23a81296ddc08a4ffa8c43b16f7617d828e153f5
Zachary Nicolaou <zack.nicolaou@gmail.com>
2021-09-08 12:54:44 -0700

    added pendula jacobian

--- auto/pendula/pendula.c
+++ auto/pendula/pendula.c
@@ -42,25 +42,89 @@ int func (integer ndim, const doublereal *w, const integer *icp,
     return 0;
   }
 
-  //TODO: add jacobian
-  //ARRAY2D(dfdu,i,j)=
-  // for(j=0; j<N; j++){
-  //   f[0*N+j]=-p[j]*y[j]/(1+delta)/omega+gamma*(1-x[j]*x[j]-y[j]*y[j])*x[j];
-  //   f[1*N+j]= p[j]*x[j]/(1+delta)/omega+gamma*(1-x[j]*x[j]-y[j]*y[j])*y[j];
-  //   f[2*N+j]=(-eta*p[j]-(1+omega*omega*amp*X+4*delta)*y[j]+(1-delta)*(x[j]*v[j]-u[j]*y[j]+x[j]*v[(j-1+N)%N]-y[j]*u[(j-1+N)%N]))/omega;
-  //   f[3*N+j]=-q[j]*v[j]/(1-delta)/omega+gamma*(1-u[j]*u[j]-v[j]*v[j])*u[j];
-  //   f[4*N+j]= q[j]*u[j]/(1-delta)/omega+gamma*(1-u[j]*u[j]-v[j]*v[j])*v[j];
-  //   f[5*N+j]=(-eta*q[j]-(1+omega*omega*amp*X-4*delta)*v[j]+(1+delta)*(u[j]*y[j]-x[j]*v[j]+u[j]*y[(j+1)%N]-v[j]*x[(j+1)%N]))/omega;
-  // }
+  for(j=0; j<N; j++){
+    // f[0*N+j]=-p[j]*y[j]/(1+delta)/omega+gamma*(1-x[j]*x[j]-y[j]*y[j])*x[j];
+    ARRAY2D(dfdu,0*N+j,0*N+j)=gamma*(1-3*x[j]*x[j]-y[j]*y[j]);
+    ARRAY2D(dfdu,0*N+j,1*N+j)=-p[j]/(1+delta)/omega+gamma*(-2*y[j])*x[j];
+    ARRAY2D(dfdu,0*N+j,2*N+j)=-y[j]/(1+delta)/omega;
+
+    // f[1*N+j]= p[j]*x[j]/(1+delta)/omega+gamma*(1-x[j]*x[j]-y[j]*y[j])*y[j];
+    ARRAY2D(dfdu,1*N+j,0*N+j)=p[j]/(1+delta)/omega+gamma*(-2*x[j])*y[j];
+    ARRAY2D(dfdu,1*N+j,1*N+j)=gamma*(1-x[j]*x[j]-3*y[j]*y[j]);
+    ARRAY2D(dfdu,1*N+j,2*N+j)=x[j]/(1+delta)/omega;
+
+    // f[2*N+j]=(-eta*p[j]-(1+omega*omega*amp*X+4*delta)*y[j]+(1-delta)*(x[j]*v[j]-u[j]*y[j]+x[j]*v[(j-1+N)%N]-y[j]*u[(j-1+N)%N]))/omega;
+    ARRAY2D(dfdu,2*N+j,0*N+j)=((1-delta)*(v[j]+v[(j-1+N)%N]))/omega;
+    ARRAY2D(dfdu,2*N+j,1*N+j)=(-(1+omega*omega*amp*X+4*delta)+(1-delta)*(-u[j]-u[(j-1+N)%N]))/omega;
+    ARRAY2D(dfdu,2*N+j,2*N+j)=(-eta)/omega;
+    ARRAY2D(dfdu,2*N+j,3*N+j)=((1-delta)*(-y[j]))/omega;
+    ARRAY2D(dfdu,2*N+j,4*N+j)=((1-delta)*(x[j]))/omega;
+    ARRAY2D(dfdu,2*N+j,3*N+(j-1+N)%N)=((1-delta)*(-y[j]))/omega;
+    ARRAY2D(dfdu,2*N+j,4*N+(j-1+N)%N)=((1-delta)*(x[j]))/omega;
+    ARRAY2D(dfdu,2*N+j,6*N+0)=(-(omega*omega*amp)*y[j])/omega;
+
+    // f[3*N+j]=-q[j]*v[j]/(1-delta)/omega+gamma*(1-u[j]*u[j]-v[j]*v[j])*u[j];
+    ARRAY2D(dfdu,3*N+j,3*N+j)=gamma*(1-3*u[j]*u[j]-v[j]*v[j]);
+    ARRAY2D(dfdu,3*N+j,4*N+j)=-q[j]/(1-delta)/omega+gamma*(-2*v[j])*u[j];
+    ARRAY2D(dfdu,3*N+j,5*N+j)=-v[j]/(1-delta)/omega;
+
+    // f[4*N+j]= q[j]*u[j]/(1-delta)/omega+gamma*(1-u[j]*u[j]-v[j]*v[j])*v[j];
+    ARRAY2D(dfdu,4*N+j,3*N+j)=q[j]/(1-delta)/omega+gamma*(-2*u[j])*v[j];
+    ARRAY2D(dfdu,4*N+j,4*N+j)=gamma*(1-u[j]*u[j]-3*v[j]*v[j]);
+    ARRAY2D(dfdu,4*N+j,5*N+j)=u[j]/(1-delta)/omega;
+
+    // f[5*N+j]=(-eta*q[j]-(1+omega*omega*amp*X-4*delta)*v[j]+(1+delta)*(u[j]*y[j]-x[j]*v[j]+u[j]*y[(j+1)%N]-v[j]*x[(j+1)%N]))/omega;
+    ARRAY2D(dfdu,5*N+j,0*N+j)=((1+delta)*(-v[j]))/omega;
+    ARRAY2D(dfdu,5*N+j,1*N+j)=((1+delta)*(u[j]))/omega;
+    ARRAY2D(dfdu,5*N+j,0*N+(j+1)%N)=((1+delta)*(-v[j]))/omega;
+    ARRAY2D(dfdu,5*N+j,1*N+(j+1)%N)=((1+delta)*(u[j]))/omega;
+    ARRAY2D(dfdu,5*N+j,3*N+j)=((1+delta)*(y[j]+y[(j+1)%N]))/omega;
+    ARRAY2D(dfdu,5*N+j,4*N+j)=(-(1+omega*omega*amp*X-4*delta)+(1+delta)*(-x[j]-x[(j+1)%N]))/omega;
+    ARRAY2D(dfdu,5*N+j,5*N+j)=(-eta)/omega;
+    ARRAY2D(dfdu,5*N+j,6*N+0)=(-(omega*omega*amp)*v[j])/omega;
+
+  }
+
   // f[6*N+0]=-Y+gamma*(1-X*X-Y*Y)*X;
+  ARRAY2D(dfdu,6*N+0,6*N+0)=gamma*(1-3*X*X-Y*Y);
+  ARRAY2D(dfdu,6*N+0,6*N+1)=-1+gamma*(-2*Y)*X;
+
   // f[6*N+1]= X+gamma*(1-X*X-Y*Y)*Y;
+  ARRAY2D(dfdu,6*N+1,6*N+0)=1+gamma*(-2*X)*Y;
+  ARRAY2D(dfdu,6*N+1,6*N+1)=gamma*(1-X*X-3*Y*Y);
+
 
   if (ijac == 1) {
     return 0;
   }
 
-  //TODO: add jacobian
-  //ARRAY2D(dfdp,i,j)=
+  for(j=0; j<N; j++){
+    // f[0*N+j]=-p[j]*y[j]/(1+delta)/omega+gamma*(1-x[j]*x[j]-y[j]*y[j])*x[j];
+    ARRAY2D(dfdp,0*N+j,1)=p[j]*y[j]/(1+delta)/omega/omega;
+    ARRAY2D(dfdp,0*N+j,2)=p[j]*y[j]/(1+delta)/omega/(1+delta);
+
+    // f[1*N+j]= p[j]*x[j]/(1+delta)/omega+gamma*(1-x[j]*x[j]-y[j]*y[j])*y[j];
+    ARRAY2D(dfdp,1*N+j,1)=-p[j]*x[j]/(1+delta)/omega/omega;
+    ARRAY2D(dfdp,1*N+j,2)=-p[j]*x[j]/(1+delta)/omega/(1+delta);
+
+    // f[2*N+j]=(-eta*p[j]-(1+omega*omega*amp*X+4*delta)*y[j]+(1-delta)*(x[j]*v[j]-u[j]*y[j]+x[j]*v[(j-1+N)%N]-y[j]*u[(j-1+N)%N]))/omega;
+    ARRAY2D(dfdp,2*N+j,0)=(-(omega*omega*X)*y[j])/omega;
+    ARRAY2D(dfdp,2*N+j,1)=(eta*p[j]-(-1+omega*amp*X-4*delta)*y[j]-(1-delta)*(x[j]*v[j]-u[j]*y[j]+x[j]*v[(j-1+N)%N]-y[j]*u[(j-1+N)%N]))/omega/omega;
+    ARRAY2D(dfdp,2*N+j,2)=(-(4)*y[j]+(-1)*(x[j]*v[j]-u[j]*y[j]+x[j]*v[(j-1+N)%N]-y[j]*u[(j-1+N)%N]))/omega;
+
+    // f[3*N+j]=-q[j]*v[j]/(1-delta)/omega+gamma*(1-u[j]*u[j]-v[j]*v[j])*u[j];
+    ARRAY2D(dfdp,3*N+j,1)=q[j]*v[j]/(1-delta)/omega/omega;
+    ARRAY2D(dfdp,3*N+j,2)=-q[j]*v[j]/(1-delta)/omega/(1-delta);
+
+    // f[4*N+j]= q[j]*u[j]/(1-delta)/omega+gamma*(1-u[j]*u[j]-v[j]*v[j])*v[j];
+    ARRAY2D(dfdp,4*N+j,1)=-q[j]*u[j]/(1-delta)/omega/omega;
+    ARRAY2D(dfdp,4*N+j,2)=q[j]*u[j]/(1-delta)/omega/(1-delta);
+
+    // f[5*N+j]=(-eta*q[j]-(1+omega*omega*amp*X-4*delta)*v[j]+(1+delta)*(u[j]*y[j]-x[j]*v[j]+u[j]*y[(j+1)%N]-v[j]*x[(j+1)%N]))/omega;
+    ARRAY2D(dfdp,5*N+j,0)=(-(omega*omega*X)*v[j])/omega;
+    ARRAY2D(dfdp,5*N+j,1)=(eta*q[j]-(-1+omega*amp*X+4*delta)*v[j]-(1+delta)*(u[j]*y[j]-x[j]*v[j]+u[j]*y[(j+1)%N]-v[j]*x[(j+1)%N]))/omega/omega;
+    ARRAY2D(dfdp,5*N+j,2)=(-(-4)*v[j]+(1)*(u[j]*y[j]-x[j]*v[j]+u[j]*y[(j+1)%N]-v[j]*x[(j+1)%N]))/omega;
+  }
 
   return 0;
 }
